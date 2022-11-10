49bbacdc0dba6a76841d287417ca40c2f23cdd6a
Vladislav Bunduki <69432124+VladPendalf@users.noreply.github.com>
2022-11-10 08:32:17 +0300

    Update priorityqueue.cpp

--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -17,8 +17,9 @@ PriorityQueue::~PriorityQueue()
         Node* temp = head;
         while (head)
         {
-            head = head->next;
             delete temp;
+            temp = head;
+            head = head->next;
         }
     }
 }
